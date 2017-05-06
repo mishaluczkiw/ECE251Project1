482a169b42eb72d2d0d6d931a5744d2cac1f9be8
varouzan <varouzankn@gmail.com>
2017-05-06 18:52:22 -0400

    Update main.c

--- TIMER/src/main.c
+++ TIMER/src/main.c
@@ -29,15 +29,9 @@ volatile double laps[5];
 
 SI_INTERRUPT_USING (TIMER4_ISR, TIMER4_IRQn, 1)
 {
-
-	//TMR4CN0_TF4H = 0;
-
-
-	    TMR4CN0_TF4H = 0;
-		//TMR4CN0 &= ~TMR3CN0_TF3H__BMASK;
-
-		time+=0.0307692;
-		lap+=0.0307692;
+	 TMR4CN0_TF4H = 0;		
+	time+=0.0307692;
+	lap+=0.0307692;
 
 }
 volatile int index = 0;
@@ -46,23 +40,10 @@ SI_INTERRUPT(PMATCH_ISR, PMATCH_IRQn)
 {
   uint8_t SFRPAGE_save = SFRPAGE;
 
- // if ((BSP_PB0 == BSP_PB_PRESSED)==false && (BSP_PB1 == BSP_PB_PRESSED)==false ){
-     	//		   pressed=false;
-     //		   }
-  //if((BSP_PB0 == BSP_PB_PRESSED))// && !pressed)// || (BSP_PB1 == BSP_PB_PRESSED && !pressed))
- //{
-	  //pressed=true;
   	  pressed=true;
 
-
-//  }
-  //LEDG=0;
   SFRPAGE = PG2_PAGE;
-
-  //EIE1 &= ~0x02;                     // Disable Port Match interrupts to
-                                     // prevent multiple interrupts from
-                                     // occurring while the button is
-                                     // pressed
+                    
   SFRPAGE = SFRPAGE_save;
 }
 
@@ -81,14 +62,10 @@ void main(void)
 	// Enable all interrupts
 	DISP_Init();
 	LEDG=0;
-	IE_EA = 1;
-	//UI_getPowerMode(0);
-	// DrawScreenText(powerModeText[0], TEXT_ROW0_Y);
-
+	IE_EA = 1;	
 	 sprintf(ar,"LAP time %.2f sec",time);
 	 DrawScreenText(ar, 10);
 	 while(true){
-		 //pressed=false;
 		 IE_EA=1;
 		 WDTCN = 0xA5;
 		 sprintf(ar,"TOT time %.2f sec",time);
